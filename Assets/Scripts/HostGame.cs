658e9a7fca4391f4e6f7939aeafe3f3e65d0f35e
Abhishek <abhishekm942@gmail.com>
2021-01-26 22:53:37 +0530

    Added UI

---
+++ Assets/Scripts/HostGame.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+    [SerializeField]
+    uint roomSize = 4;
+    string roomName;
+    NetworkManager networkManager;
+
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomName(string name)
+    {
+        roomName = name;
+    }
+
+    public void CreateRoom()
+    {
+        if (roomName != null && roomName != "")
+        {
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+}
