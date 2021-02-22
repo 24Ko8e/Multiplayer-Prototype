02e9bca156d1402c9aa5821aa555b3c5fecfa25c
Abhishek <abhishekm942@gmail.com>
2021-02-22 12:01:08 +0530

    Added player nameplate

---
+++ Assets/Scripts/PlayerNameplate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameplate : MonoBehaviour
+{
+    [SerializeField]
+    Text usernameText;
+
+    [SerializeField]
+    RectTransform healthBarFill;
+
+    [SerializeField]
+    Player player;
+
+    Camera m_Camera;
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        m_Camera = Camera.main;
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward, m_Camera.transform.rotation * Vector3.up);
+
+        usernameText.text = player.username;
+        healthBarFill.localScale = new Vector3(player.GetHealthPercentage(), 1f, 1f);
+    }
+}
